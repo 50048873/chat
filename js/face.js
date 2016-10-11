aa36411e62d59942713393d3717c2dbec29ca449
50048873 <50048873@qq.com>
2016-10-11 20:40:12 +0800

    测试contenteditable属性

--- js/face.js
+++ js/face.js
@@ -9,25 +9,30 @@
 	/*
 	*		参数说明
 	*		dir:		【数组】保存表情包文件夹名字
-	*		num:		【数组】保存表情包表情个数
+	*		num:		【数组】保存表情每个分类包表情个数
+			category:	【数组】保存分类
+			isLoaded:	【数组】保存是否加载过
 	*/
 	var defaults = { 
 		dir: ['mr', 'gnl', 'lxh', 'bz'],
 		num: [75, 46, 83, 70],
-		category: ['默认', '拜年', '浪花', '暴走']
+		category: ['默认', '拜年', '浪花', '暴走'],
+		isLoaded: [false, false, false, false]
 	};
 
 	function Face(wrap, options) { 
 		this.$wrap = $(wrap);
 		this.opts = $.extend({}, defaults, options);
+		this.index = 0;
 
 		this.init();
 	};
 
 	Face.prototype.init = function() { 
 		this.initNav();
-		this.loadIcons(0);
+		this.loadIcons(this.index);
 		this.toggleCategory();
+		this.chooseFace();
 	};
 
 	Face.prototype.initNav = function() { 
@@ -46,25 +51,36 @@
 	Face.prototype.loadIcons = function(index) { 
 		var dir = this.opts.dir;
 		var num = this.opts.num;
+		var isLoaded = this.opts.isLoaded;
 		var arr = [];
 		for (var i = 1; i < num[index]; i++) { 
-			arr.push('<img src="img/' + dir[index] + '/' + i + '.gif">');
+			arr.push('<img src="img/' + dir[index] + '/' + i + '.gif" alt="' + i + '.gif">');
 		}
-		this.$wrap.prepend('<div class="clearfix category">' + arr.join('') + '</div>');
+		this.$wrap.append('<div class="clearfix category">' + arr.join('') + '</div>');
+		isLoaded[index] = true;
 	};
 
 	Face.prototype.toggleCategory = function() { 
-		var index = 0;
 		var _this = this;
+		var isLoaded = this.opts.isLoaded;
 		$('.f-nav').click(function(e) { 
 			var a = e.target;
 			if (a.tagName == 'A') { 
 				e.preventDefault();
-				index = $(a).index();
+				_this.index = $(a).index();
 				$(a).addClass('selected').siblings().removeClass('selected');
-				//$('.category').eq(index).show().siblings('.category').hide();
-				_this.loadIcons(index);
+				if (isLoaded[_this.index] == false) { 
+					_this.loadIcons(_this.index);
+				}
+				$('.category').eq(_this.index).show().siblings('.category').hide();
 			}
+		});
+	};
+
+	Face.prototype.chooseFace = function() { 
+		$('.category').click(function(e) { 
+			var img = e.target;
+			$('#enterBox').append(img);
 		});
 	};
 
